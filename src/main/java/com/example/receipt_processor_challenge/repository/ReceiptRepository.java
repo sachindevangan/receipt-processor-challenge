61037350252bba346ac3630893fb521f1a5a3758
Sachin Devangan <dsachin.98@gmail.com>
2025-04-13 20:41:48 -0500

    Add in-memory receipt repository

---
+++ src/main/java/com/example/receipt_processor_challenge/repository/ReceiptRepository.java
@@ -0,0 +1,28 @@
+package com.example.receipt_processor_challenge.repository;
+
+
+import com.example.receipt_processor_challenge.model.Receipt;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class ReceiptRepository {
+    private final Map<String, Receipt> receiptStore = new ConcurrentHashMap<>();
+
+    public String storeReceipt(Receipt receipt) {
+        String id = UUID.randomUUID().toString();
+        receiptStore.put(id, receipt);
+        return id;
+    }
+
+    public Receipt getReceipt(String id) {
+        return receiptStore.get(id);
+    }
+
+    public Map<String, Receipt> getAllReceipts() {
+        return receiptStore;
+    }
+}
